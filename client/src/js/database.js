35d2232f601ab8e9d6bab46020612f20fe6e4f13
Jessica Almand <jlalmand27@gmail.com>
2022-12-02 16:25:53 -0500

    Added logic to methods and workbox plugin

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.log('putDb implemented');
+  const textEdDb = await openDB('textEdDb', 1);
+  const tx = contactDb.transaction('textEdDb', 'readwrite');
+  const store = tx.objectStore('textEdDb');
+  const request = store.put({ id: 1, textEdDb: content });
+  const result = await request;
+  console.log('Saved', result);
+  return result;
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('putDb implemented');
+  const textEdDb = await openDB('textEdDb', 1);
+  const tx = contactDb.transaction('textEdDb', 'readwrite');
+  const store = tx.objectStore('textEdDb');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
